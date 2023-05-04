ca467d3dba8d91302aa8dc312ce361cc413586e7
hwang seung joon <asherolleh@gmail.com>
2023-05-04 12:59:48 +0900

    Project Create

---
+++ src/main/kotlin/com/example/authentication/authentication_jwt/AuthenticationJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.authentication.authentication_jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthenticationJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthenticationJwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/authentication/authentication_jwt/AuthenticationJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.authentication.authentication_jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthenticationJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
