db5c373ed9c77199039609806e289259a21eef2b
hwang seung joon <asherolleh@gmail.com>
2023-07-24 22:29:53 +0900

    email vo 생성 동반 객체를 이용해 생성

---
+++ src/main/kotlin/com/example/authentication/authentication_jwt/domain/user/Email.kt
@@ -0,0 +1,19 @@
+package com.example.authentication.authentication_jwt.domain.user
+
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Email private constructor(
+    var email: String
+) {
+    companion object {
+        private const val localPattern = "([a-z0-9!#\\\$%&'*+/=?^_`{|}~-]+\\.?[a-z0-9!#\\\$%&'*+/=?^_`{|}~-]+)"
+
+        private const val domainPattern = "[a-zA-Z0-9-]+\\.[a-zA-Z0-9-]+"
+
+        private const val emailPattern = "^${localPattern}@${domainPattern}$"
+
+        fun validEmail(userInput: String): Email =
+            if(userInput.length <= 200 && Regex(emailPattern).matches(userInput)) Email(userInput) else throw Exception()
+    }
+}
--- src/main/kotlin/com/example/authentication/authentication_jwt/domain/user/User.kt
+++ src/main/kotlin/com/example/authentication/authentication_jwt/domain/user/User.kt
@@ -1,11 +1,39 @@
 package com.example.authentication.authentication_jwt.domain.user
 
+import com.example.authentication.authentication_jwt.domain.core.PrimaryKeyEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Embedded
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import java.time.LocalDateTime
 
-//@Entity
-//@Table(name = "user")
-//class User(
-//
-//): PrimaryKeyEntity {
-//}
+@Entity
+@Table(name = "user")
+class User(
+    email: Email?,
+    phoneNumber: String?,
+    name: String,
+    imgUrl: String?,
+): PrimaryKeyEntity() {
+
+    @Column(nullable = true, unique = true)
+    @Embedded
+    var email: Email? = email
+        protected set
+
+    @Column(nullable = true, unique = true)
+    var phoneNumber: String? = phoneNumber
+        protected set
+
+    @Column(nullable = false, unique = true)
+    var name: String = name
+        protected set
+
+    @Column(nullable = true)
+    var imgUrl: String? = imgUrl
+
+    @Column(nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+
+}
