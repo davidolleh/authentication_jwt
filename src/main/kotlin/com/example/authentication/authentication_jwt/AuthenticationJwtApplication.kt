97d88c0dd075f4832d8cc75170c80febaabd66df
hwang seung joon <asherolleh@gmail.com>
2024-02-21 14:48:31 +0900

    [feat] jwt secret key 값 세팅

--- src/main/kotlin/com/example/authentication/authentication_jwt/AuthenticationJwtApplication.kt
+++ src/main/kotlin/com/example/authentication/authentication_jwt/AuthenticationJwtApplication.kt
@@ -1,6 +1,5 @@
 package com.example.authentication.authentication_jwt
 
-import com.example.authentication.authentication_jwt.domain.verification.EmailService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
