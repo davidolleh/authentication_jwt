cb44d50410eab4bad21460bd1da4e6193be3de72
hwang seung joon <asherolleh@gmail.com>
2024-02-21 14:44:53 +0900

    fix java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.6"
-	id("io.spring.dependency-management") version "1.1.0"
-	id ("org.jetbrains.kotlin.plugin.allopen") version "1.7.22"
-	id ("org.jetbrains.kotlin.plugin.noarg") version "1.7.22"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
-	kotlin("plugin.jpa") version "1.7.22"
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+	id ("org.jetbrains.kotlin.plugin.allopen") version "1.8.22"
+	id ("org.jetbrains.kotlin.plugin.noarg") version "1.8.22"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
 }
 
 allOpen {
@@ -35,12 +35,18 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 //	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 //	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	implementation("io.jsonwebtoken:jjwt:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+
 
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -59,6 +65,7 @@ dependencies {
 	//eclipselink kotlin jdsl and eclipselink package
 //	implementation("com.linecorp.kotlin-jdsl:eclipselink-kotlin-jdsl-jakarta:x.y.z")
 //	implementation("org.eclipse.persistence:org.eclipse.persistence.jpa:4.y.z")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile> {
@@ -72,3 +79,11 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
